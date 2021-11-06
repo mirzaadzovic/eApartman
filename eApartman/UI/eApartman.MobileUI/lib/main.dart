dee69ad675dcb65ca2f75ce3a84c9317a8bdc48d
Mirza <katarza.21@gmail.com>
2021-11-06 19:42:52 +0100

    FLutter Login kao početna stranica

--- eApartman/UI/eApartman.MobileUI/lib/main.dart
+++ eApartman/UI/eApartman.MobileUI/lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'eApartman',
-      home: Home(),
+      home: Login(),
       routes: {
         '/home': (context) => const Home(),
       },
